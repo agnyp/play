6ce71846a18ce585fe2e7ff473360a12ecdd6ae1
Evan Lind <evan.p.lind@gmail.com>
2013-03-07 14:55:48 -0800

    twitch live

--- js/ops_jtv.js
+++ js/ops_jtv.js
@@ -1,19 +1,10 @@
-var url = "https://api.twitch.tv/kraken/streams";
+var url = "http://api.justin.tv/api/stream/list.json";
 var stream = "?channel=tobiwandota,versuta,minidota,wagamamatv,netolicrc,sexybamboe,dendi,wepla,alaito&callback=?";
 var streamSelection = [];
 var streamDota = ["dendi","tobiwandota","starladder1","wagamamatv","onemoregametv2","versuta","4cejkee","beyondthesummit","minidota"];
 var streamWoW = ["styrka","affinitiibl","amiye","slootbag","zuperwtf","killars","syiler","sco","healthbar"];
 var streamProm = ["styrka","greenyb","healthbar","velna","amiye","kynks"];
 var streamdata = [];
-
-var flashHeight = 295;
-var flashWidth = 353;
-var flashType = "application/x-shockwave-flash";
-var flashData = "http://www.twitch.tv/widgets/live_embed_player.swf";
-var flashColor = "#000"
-var flashFS = "true";
-var flashSA = "always";
-var flashVars = "auto_play=false&start_volume=25&channel=";
 
 function removeA(arr) {
   var what, a = arguments, L = a.length, ax;
@@ -62,27 +53,24 @@ function loadStreams() {
   $.getJSON(url + stream, function(data) {
     var datact = 0;
     $.each(data, function(id, node) {
-      if (id==='streams') {
-        datact = node.length;
-        for (i=0; i<datact; i++) {
-          streamdata[i] = [];
-          $.each(node[i], function(key, val) {
-            if (key==='viewers') streamdata[i].viewers = val;
-            if (key==='channel') {
-              streamdata[i].name = val.name;
-              streamdata[i].game = val.game;
-              streamdata[i].title = val.status;
-            }
-          });
-        };
-      };
+        streamdata[datact] = [];
+        $.each(node, function(key, val) {
+          if (key==='title') streamdata[datact].title = val;
+          if (key==='stream_count') streamdata[datact].viewers = val;
+          if (key==='meta_game') streamdata[datact].game = val;
+          if(key==='channel') {
+            streamdata[datact].stream = val.embed_code;
+            streamdata[datact].name = val.login;
+          }
+        });
+        datact++;
     });
    $('#streams').html('');
     for (i=0;i<datact;i++) {
       if (i%3===0) {
         $('#streams').append('<article class="row">');
       }
-      streamCreate(streamdata[i]);
+      $('#streams').children().last().append('<section id="' + streamdata[i].name + '" class="one third padded"> <h3>' + streamdata[i].name + '</h3>' + streamdata[i].stream + '<p>' + streamdata[i].title + '</p> </section>');
       if (i%3===2) {
         $('#streams').append('</article>');
       }
@@ -91,10 +79,6 @@ function loadStreams() {
       $('#streams').append('</article>');
     }
   });
-}
-
-function streamCreate(streamA) {
-  $('#streams').children().last().append('<section id="' + streamA.name + '" class="one third padded"> <h3>' + streamA.name + '</h3>' + '<object type="' + flashType + '" height="' + flashHeight + '" width="' + flashWidth + '" id="live_embed_player_flash" data="'+ flashData + '?channel=' + name + '" bgcolor="' + flashColor + '"><param name="allowFullScreen" value="' + flashFS + '" /><param name="allowScriptAccess" value="' + flashSA + '" /><param name="movie" value="' + flashData + '" /><param name="flashvars" value="' + flashVars + streamA.name + '" /></object>' + '<p>' + streamA.title + '</p> </section>');
 }
 
 $('document').ready(loadStreams());
