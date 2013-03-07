8806c1bb0596d032694ea00b434b723239ef194d
Evan Lind <evan.p.lind@gmail.com>
2013-03-07 14:48:45 -0800

    twitch api

--- js/test2.js
+++ js/test2.js
@@ -1,24 +1,29 @@
 var url = "https://api.twitch.tv/kraken/streams?channel=";
-var stream = "sodapoppin,alinity&callback=?"
+var stream = "sodapoppin,affinitiibl&callback=?"
 
 $.getJSON(url + stream, function(data) {
   var items = [];
   $.each(data, function(id, node) {
       items.push('<li> ENTRY </li>');
+      var typea = node.length;
+      items.push('<li>' + id + ':' + node + ':' + typea + '</li>');
       $.each(node, function(key, val) {
-        items.push('<li>' + key + ':' + val + '</li>');
-          items.push('<li> CHANNEL INFO </li>');
+        var type = typeof val;
+        items.push('<li>' + key + ':' + val + ':' + type + '</li>');
+        if (typeof val === 'object') {
+          items.push('<li> STREAMS INFO </li>');
           $.each(val, function(item, iden) {
             items.push('<li>' + item + ':' + iden + '</li>');
             if(item==='channel') {
-              items.push('<li> CONTENTS </li>');
+              items.push('<li> CHANNEL INFO </li>');
               $.each(iden, function(one, two) {
                 items.push('<li>' + one + ':' + two + '</li>');
               });
-              items.push('<li> END CONTENTS </li>');
+              items.push('<li> END CHANNEL INFO </li>');
             };
           });
-          items.push('<li> END CHANNEL INFO </li>');
+          items.push('<li> END STREAMS INFO </li>');
+        };
       });
   });
  
